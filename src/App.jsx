9c8c50541116493a1c41f48e98d4796477848688
Joel <joeldome17@gmail.com>
2025-01-17 16:24:10 -0600

    initial project structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return(
+    <>
+      <div className="h-screen flex justify-center flex-col items-center">
+        <h1 className="mb-2">Hola Mundo!</h1>
+        <button className="bg-slate-200 px-4 py-2 rounded hover:bg-slate-300">Change Theme</button>
+      </div>
+
+    </>
+  )
+}
+
+export default App;
