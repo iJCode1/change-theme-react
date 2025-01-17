a43b60146271260bcc1ea76659194b0568828955
Joel <joeldome17@gmail.com>
2025-01-17 16:49:44 -0600

    change of theme between light and black

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+import { useState, useEffect } from "react";
+
 const App = () => {
+  const [theme, setTheme] = useState(() => {
+    if(window.matchMedia('(prefers-color-scheme: dark').matches){
+      return 'dark'
+    }
+
+    return 'light'
+  });
+
+  useEffect(() => {
+    if(theme === 'dark'){
+      document.querySelector('html').classList.add('dark');
+    }else{
+      document.querySelector('html').classList.remove('dark');
+    }
+  }, [theme])
+
+  const handleChangeTheme = () => {
+    console.log("change");
+    setTheme( theme === 'light' ? 'dark' : 'light');
+  }
   return(
     <>
-      <div className="h-screen flex justify-center flex-col items-center">
-        <h1 className="mb-2">Hola Mundo!</h1>
-        <button className="bg-slate-200 px-4 py-2 rounded hover:bg-slate-300">Change Theme</button>
+      <div className="h-screen flex justify-center flex-col items-center dark:bg-slate-900 dark:text-white">
+        <p className="mb-2 text-xl">Hola Mundo!</p>
+        <button className="bg-slate-200 px-4 py-2 rounded hover:bg-slate-300 dark:text-white dark:bg-slate-700 dark:hover:bg-slate-800" onClick={handleChangeTheme}>Change Theme</button>
       </div>
-
     </>
   )
 }
